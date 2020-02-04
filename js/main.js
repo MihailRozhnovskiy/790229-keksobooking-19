9108327816bc8c7f11e665c0491256b29cbafb1b
MihailRozhnovskiy <mihail.exp@gmail.com>
2020-02-04 14:30:34 +0200

    Добавляет Личный проект: больше деталей (часть 2)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var QUANTITY_MOCKS = 8;
+
 var renderMocks = function (n) {
 
   var mocks = [];
@@ -8,9 +10,9 @@ var renderMocks = function (n) {
   var checkoutTimes = ['12:00', '13:00', '14:00'];
   var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
   var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-  var width = 1200;
-  var minHeight = 130;
-  var maxHeight = 630;
+  var widthMap = document.querySelector('body').clientWidth;
+  var MIN_HEIGHT = 130;
+  var MAX_HEIGHT = 630;
 
   var getItems = function (items) {
     var randomItems = [];
@@ -43,18 +45,21 @@ var renderMocks = function (n) {
     mock.offer.features = getItems(features);
     mock.offer.description = 'String description';
     mock.offer.photos = getItems(photos);
-    mock.location.x = Math.floor(Math.random() * Math.floor(width));
-    mock.location.y = Math.floor(minHeight + Math.random() * (maxHeight + 1 - minHeight));
+    mock.location.x = Math.floor(Math.random() * Math.floor(widthMap));
+    mock.location.y = Math.floor(MIN_HEIGHT + Math.random() * (MAX_HEIGHT + 1 - MIN_HEIGHT));
 
     mocks.push(mock);
   }
   return mocks;
 };
 
-var pins = renderMocks(8);
+var pins = renderMocks(QUANTITY_MOCKS);
 
+var map = document.querySelector('.map');
+var filtersContainer = document.querySelector('.map__filters-container');
 var mapPins = document.querySelector('.map__pins');
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
 
 var renderPin = function (mock) {
   var pinElement = pinTemplate.cloneNode(true);
@@ -66,6 +71,82 @@ var renderPin = function (mock) {
   pinElement.firstChild.setAttribute('alt', mock.offer.title);
   return pinElement;
 };
+
+var renderCard = function (mock) {
+  var cardElement = cardTemplate.cloneNode(true);
+  var type = '';
+  var counter = 0;
+  var COUNT = 6;
+  switch (mock.offer.type) {
+    case 'palace':
+      type = 'Дворец';
+      break;
+    case 'flat':
+      type = 'Квартира';
+      break;
+    case 'house':
+      type = 'Дом';
+      break;
+    default:
+      type = 'Бунгало';
+  }
+  var getListFeatures = function (items) {
+    var fragment = document.createDocumentFragment();
+
+    for (var i = 0; i < items.length; i++) {
+      var li = document.createElement('li');
+      if (items[i] === 'wifi') {
+        li.className = 'popup__feature popup__feature--wifi';
+      }
+      if (items[i] === 'dishwasher') {
+        li.className = 'popup__feature popup__feature--dishwasher';
+      }
+      if (items[i] === 'parking') {
+        li.className = 'popup__feature popup__feature--parking';
+      }
+      if (items[i] === 'washer') {
+        li.className = 'popup__feature popup__feature--washer';
+      }
+      if (items[i] === 'elevator') {
+        li.className = 'popup__feature popup__feature--elevator';
+      }
+      if (items[i] === 'conditioner') {
+        li.className = 'popup__feature popup__feature--conditioner';
+      }
+      fragment.append(li);
+    }
+    return fragment;
+  };
+
+  var getListPhotos = function (items) {
+    var fragment = document.createDocumentFragment();
+
+    for (var i = 0; i < items.length; i++) {
+      var img = document.createElement('img');
+      img.setAttribute('src', items[i]);
+      fragment.append(img);
+    }
+    return fragment;
+  };
+
+  cardElement.querySelector('.popup__title').textContent = mock.offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = mock.offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = mock.offer.price + ' ₽ /ночь';
+  cardElement.querySelector('.popup__type').textContent = type;
+  cardElement.querySelector('.popup__text--capacity').textContent = mock.offer.rooms + ' комнаты для ' + mock.offer.guests + ' гостей';
+  cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + mock.offer.checkin + ', выезд до ' + mock.offer.checkout;
+  while (counter < COUNT) {
+    cardElement.querySelector('.popup__features').removeChild(cardElement.querySelector('.popup__feature'));
+    counter++;
+  }
+  cardElement.querySelector('.popup__features').append(getListFeatures(mock.offer.features));
+  cardElement.querySelector('.popup__description').textContent = mock.offer.description;
+  cardElement.querySelector('.popup__photos').append(getListPhotos(mock.offer.photos));
+  cardElement.querySelector('.popup__avatar').setAttribute('src', mock.author.avatar);
+  return cardElement;
+};
+
+map.insertBefore(renderCard(pins[0]), filtersContainer);
 
 var drawPin = function () {
   var fragment = document.createDocumentFragment();
@@ -82,5 +163,4 @@ var openMap = function () {
 };
 
 openMap();
-
 
