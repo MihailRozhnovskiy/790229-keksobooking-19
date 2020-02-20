f3282b87e09e883f32c9024fa265bb5eb5310d5a
MihailRozhnovskiy <mihail.exp@gmail.com>
2020-02-20 10:53:00 +0200

    Добавляет валидацию формы

--- js/main.js
+++ js/main.js
@@ -185,6 +185,51 @@ var buttonPinMainPushHandler = function (evt) {
 buttonPinMain.addEventListener('mousedown', buttonPinMainPushHandler);
 buttonPinMain.addEventListener('keydown', buttonPinMainPushHandler);
 
+//
+var formTitle = document.querySelector('#title');
+
+var formTitleInputHandler = function () {
+  if (formTitle.validity.tooShort) {
+    formTitle.setCustomValidity('Минимальная длина — 30 символов');
+  } else if (formTitle.validity.tooLong) {
+    formTitle.setCustomValidity('Максимальная длина — 100 символов');
+  } else if (formTitle.validity.valueMissing) {
+    formTitle.setCustomValidity('Обязательное текстовое поле');
+  } else {
+    formTitle.setCustomValidity('');
+  }
+};
+formTitle.addEventListener('invalid', formTitleInputHandler);
+
+var selectTypeLodging = document.querySelector('#type');
+var formPrice = document.querySelector('#price');
+
+var compTypeLodgingPriceHandler = function () {
+  var typeLodging = document.querySelector('#type').value;
+  var price = document.querySelector('#price').value;
+  var MIN_PRICE_BUNGALO = 0;
+  var MIN_PRICE_FLAT = 1000;
+  var MIN_PRICE_HOUSE = 5000;
+  var MIN_PRICE_PALACE = 10000;
+  var MAX_PRICE = 1000000;
+  if (typeLodging === 'bungalo' && price < MIN_PRICE_BUNGALO) {
+    formPrice.setCustomValidity('«Бунгало» — минимальная цена за ночь 0');
+  } else if (typeLodging === 'flat' && price < MIN_PRICE_FLAT) {
+    formPrice.setCustomValidity('«Квартира» — минимальная цена за ночь 1 000');
+  } else if (typeLodging === 'house' && price < MIN_PRICE_HOUSE) {
+    formPrice.setCustomValidity('«Дом» — минимальная цена за ночь 5 000');
+  } else if (typeLodging === 'palace' && price < MIN_PRICE_PALACE) {
+    formPrice.setCustomValidity('«Дворец» — минимальная цена за ночь 10 000');
+  } else if (price > MAX_PRICE) {
+    formPrice.setCustomValidity('Максимальное значение — 1 000 000');
+  } else {
+    formPrice.setCustomValidity('');
+  }
+};
+formPrice.addEventListener('change', compTypeLodgingPriceHandler);
+selectTypeLodging.addEventListener('change', compTypeLodgingPriceHandler);
+
+
 var selectRoom = document.querySelector('#room_number');
 var selectGuest = document.querySelector('#capacity');
 
@@ -197,12 +242,49 @@ var compGuestsRoomsHandler = function () {
       (quantityRoom === '100' && quantityGuest !== '0')) {
     selectRoom.setCustomValidity('Количество комнат не соответствует количеству гостей');
   } else {
-    selectRoom.setCustomValidity('норм');
+    selectRoom.setCustomValidity('');
   }
 };
-
 selectRoom.addEventListener('change', compGuestsRoomsHandler);
 selectGuest.addEventListener('change', compGuestsRoomsHandler);
+
+var formAddress = document.querySelector('#address');
+
+var formAddressHandler = function () {
+  if (focus) {
+    formAddress.setCustomValidity('Ручное редактирование поля запрещено');
+  }
+};
+formAddress.addEventListener('focus', formAddressHandler);
+
+var selectTimeIn = document.querySelector('#timein');
+var selectTimeOut = document.querySelector('#timeout');
+
+var compTimeInTimeOutHandler = function () {
+  var timeIn = document.querySelector('#timein').value;
+  var timeOut = document.querySelector('#timeout').value;
+  if (parseInt(timeOut, 10) < parseInt(timeIn, 10)) {
+    selectTimeIn.setCustomValidity('');
+  } else {
+    selectTimeIn.setCustomValidity('«Время выезда» должно быть не менее чем за час до «Время заезда»');
+  }
+};
+selectTimeIn.addEventListener('change', compTimeInTimeOutHandler);
+selectTimeOut.addEventListener('change', compTimeInTimeOutHandler);
+
+
+var formAvatar = document.querySelector('#avatar');
+var formImages = document.querySelector('#images');
+
+var formAvatarImagesHandler = function () {
+  if (formAvatar.focus) {
+    formAvatar.setCustomValidity('Загрузите изображение');
+  } else if (formImages.focus) {
+    formImages.setCustomValidity('Загрузите изображение');
+  }
+};
+formAvatar.addEventListener('focus', formAvatarImagesHandler);
+formImages.addEventListener('focus', formAvatarImagesHandler);
 
 //
 var map = document.querySelector('.map');
@@ -211,11 +293,19 @@ var openPopup = function (target) {
   map.insertBefore(renderCard(pins[target]), filtersContainer);
 };
 
-var closePopupHandler = function (evt) {
+var closePopup = function () {
   var card = document.querySelector('.map__card');
-  if (evt || evt.key === 'Escape') {
-    card.remove();
+  card.remove();
+};
+
+var escClosePopupHandler = function (evt) {
+  if (evt.key === 'Escape') {
+    closePopup();
   }
+};
+
+var closePopupHandler = function () {
+  closePopup();
 };
 
 var mousedownPinOpenPopupHandler = function (evt) {
@@ -224,7 +314,7 @@ var mousedownPinOpenPopupHandler = function (evt) {
     openPopup(target);
     var buttonCloseCard = document.querySelector('.popup__close');
     buttonCloseCard.addEventListener('click', closePopupHandler);
-    buttonCloseCard.addEventListener('keydown', closePopupHandler);
+    document.addEventListener('keydown', escClosePopupHandler);
   }
 };
 map.addEventListener('click', mousedownPinOpenPopupHandler);
@@ -234,7 +324,7 @@ var keydownPinOpenPopupHandler = function (evt) {
   if (target && evt.key === 'Enter') {
     openPopup(target);
     var buttonCloseCard = document.querySelector('.popup__close');
-    buttonCloseCard.addEventListener('keydown', closePopupHandler);
+    document.addEventListener('keydown', escClosePopupHandler);
     buttonCloseCard.addEventListener('click', closePopupHandler);
   }
 };
