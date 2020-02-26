d9d208352443a8dc5e18a1ba25a7a21a6a33fd27
MihailRozhnovskiy <mihail.exp@gmail.com>
2020-02-26 17:06:36 +0200

    Добавляет новые правки Личный проект: модуляция (часть 2)

--- js/pin_main.js
+++ js/pin_main.js
@@ -2,6 +2,12 @@
 
 (function () {
   var pinMain = document.querySelector('.map__pin--main');
+  var widthMap = document.querySelector('body').clientWidth;
+  var MIN_HEIGHT = 130;
+  var MAX_HEIGHT = 630;
+  var pinMainSizeX = (pinMain.firstElementChild.width) / 2;
+  var pinMainSizeY = (pinMain.firstElementChild.height) / 2;
+
   var onMouseDownHandler = function (evt) {
     evt.preventDefault();
 
@@ -23,9 +29,15 @@
         y: moveEvt.clientY
       };
 
-      pinMain.style.top = (pinMain.offsetTop - shift.y) + 'px';
-      pinMain.style.left = (pinMain.offsetLeft - shift.x) + 'px';
+      var coordinateY = pinMain.offsetTop - shift.y;
+      var coordinateX = pinMain.offsetLeft - shift.x;
 
+      if (coordinateY > (MIN_HEIGHT - pinMainSizeY) && coordinateY < (MAX_HEIGHT + pinMainSizeY)) {
+        pinMain.style.top = coordinateY + 'px';
+      }
+      if (coordinateX < (widthMap - pinMainSizeX) && coordinateX > (widthMap - widthMap - pinMainSizeX)) {
+        pinMain.style.left = coordinateX + 'px';
+      }
       getAddress();
     };
 
@@ -45,8 +57,6 @@
     var address = document.querySelector('#address');
     var pinMainTop = pinMain.style.top;
     var pinMainLeft = pinMain.style.left;
-    var pinMainSizeX = (pinMain.firstElementChild.width) / 2;
-    var pinMainSizeY = (pinMain.firstElementChild.height) / 2;
     var addressValue = parseInt(pinMainLeft, 10) + pinMainSizeX + ', ' + (parseInt(pinMainTop, 10) + pinMainSizeY);
     address.setAttribute('value', addressValue);
     return address;
