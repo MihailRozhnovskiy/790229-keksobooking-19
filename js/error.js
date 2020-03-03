7f79b39ea399e1a3ef24e0d09b38c169b065ee76
MihailRozhnovskiy <mihail.exp@gmail.com>
2020-03-03 10:52:49 +0200

    Добавляет правки

--- js/error.js
+++ js/error.js
@@ -3,14 +3,19 @@
 (function () {
   var body = document.querySelector('body');
   var errorTemplate = document.querySelector('#error').content.querySelector('.error');
-  var buttonErrorMessage = errorTemplate.querySelector('.error__button');
+  //var buttonErrorMessage = errorTemplate.querySelector('.error__button');
 
   var openErrorMessage = function (message) {
     var errorMessage = errorTemplate.querySelector('.error__message');
-    errorMessage.innerHTML = message;
+    errorMessage.textContent = message;
     var errorElement = errorTemplate.cloneNode(true);
     body.appendChild(errorElement);
+
+    var buttonErrorMessage = errorElement.querySelector('.error__button');
+    buttonErrorMessage.addEventListener('click', closeErrorMessageHandler);
   };
+
+  //openErrorMessage('ERROR!!!!');
 
   var closeErrorMessage = function () {
     var error = body.querySelector('.error');
@@ -20,7 +25,7 @@
   var closeErrorMessageHandler = function () {
     closeErrorMessage();
   };
-  buttonErrorMessage.addEventListener('click', closeErrorMessageHandler);
+  //buttonErrorMessage.addEventListener('click', closeErrorMessageHandler);
 
   window.error = {
     openErrorMessage: openErrorMessage
