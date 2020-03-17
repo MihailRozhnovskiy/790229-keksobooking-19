bc97d6565c6aa73b224e3952bac74f0f409c4881
MihailRozhnovskiy <mihail.exp@gmail.com>
2020-03-17 10:13:12 +0200

    Добавляет правки Личный проект: перламутровые пуговицы (часть 2)

--- js/filters.js
+++ js/filters.js
@@ -3,6 +3,7 @@
 (function () {
 
   var CONST_QUANTITY_PINS = 5;
+  var TIMEOUT = 500;
 
   var getPins = function (data) {
     window.pins = data;
@@ -24,108 +25,93 @@
   var mapFilters = document.querySelector('.map__filters');
 
   var mapFiltersHandler = function () {
-    var LOW = 10000;
-    var HIGH = 50000;
-    var housingType = document.querySelector('#housing-type').value;
-    var housingPrice = document.querySelector('#housing-price').value;
-    var housingRoom = document.querySelector('#housing-rooms').value;
-    var housingGuest = document.querySelector('#housing-guests').value;
-    var selectedTypePins = [];
-    var selectedPricePins = [];
-    var selectedRoomPins = [];
-    var selectedGuestPins = [];
-    var selectedFeaturePins = [];
-    window.selectedPins = [];
+    setTimeout(function () {
+      var LOW = 10000;
+      var HIGH = 50000;
+      var housingType = document.querySelector('#housing-type').value;
+      var housingPrice = document.querySelector('#housing-price').value;
+      var housingRoom = document.querySelector('#housing-rooms').value;
+      var housingGuest = document.querySelector('#housing-guests').value;
+      var selectedTypePins = [];
+      var selectedPricePins = [];
+      var selectedRoomPins = [];
+      var selectedGuestPins = [];
+      var selectedFeaturePins = [];
+      window.selectedPins = [];
 
-    clearMap();
+      clearMap();
 
-    if (housingType === 'any') {
-      selectedTypePins = window.pins;
-    }
+      var getSelectedPins = function (housingSpecification, specification, arrSpecification, arrPins) {
+        if (housingSpecification === 'any') {
+          arrSpecification = arrPins;
+        }
+        for (var i = 0; i < arrPins.length; i++) {
+          if (String(arrPins[i].offer[specification]) === housingSpecification) {
+            arrSpecification.push(arrPins[i]);
+          }
+        }
+        window.selectedPins = arrSpecification;
+      };
+      getSelectedPins(housingType, 'type', selectedTypePins, window.pins);
 
-    for (var i = 0; i < window.pins.length; i++) {
-      if (window.pins[i].offer.type === housingType) {
-        selectedTypePins.push(window.pins[i]);
+      if (housingPrice === 'any') {
+        selectedPricePins = window.selectedPins;
       }
-    }
 
-    window.selectedPins = selectedTypePins;
-
-    if (housingPrice === 'any') {
-      selectedPricePins = window.selectedPins;
-    }
-
-    for (var j = 0; j < window.selectedPins.length; j++) {
-      if ((housingPrice === 'low' && window.selectedPins[j].offer.price < LOW) ||
+      for (var j = 0; j < window.selectedPins.length; j++) {
+        if ((housingPrice === 'low' && window.selectedPins[j].offer.price < LOW) ||
        (housingPrice === 'high' && window.selectedPins[j].offer.price > HIGH) ||
        (housingPrice === 'middle' && window.selectedPins[j].offer.price >= LOW && window.selectedPins[j].offer.price <= HIGH)) {
-        selectedPricePins.push(window.selectedPins[j]);
+          selectedPricePins.push(window.selectedPins[j]);
+        }
       }
-    }
 
-    window.selectedPins = selectedPricePins;
+      window.selectedPins = selectedPricePins;
 
-    if (housingRoom === 'any') {
-      selectedRoomPins = window.selectedPins;
-    }
+      getSelectedPins(housingRoom, 'rooms', selectedRoomPins, window.selectedPins);
 
-    for (var k = 0; k < window.selectedPins.length; k++) {
-      if (window.selectedPins[k].offer.rooms === parseInt(housingRoom, 10)) {
-        selectedRoomPins.push(window.selectedPins[k]);
-      }
-    }
-    window.selectedPins = selectedRoomPins;
+      getSelectedPins(housingGuest, 'guests', selectedGuestPins, window.selectedPins);
 
-
-    if (housingGuest === 'any') {
-      selectedGuestPins = window.selectedPins;
-    }
-
-    for (var l = 0; l < window.selectedPins.length; l++) {
-      if (window.selectedPins[l].offer.guests === parseInt(housingGuest, 10)) {
-        selectedGuestPins.push(window.selectedPins[l]);
-      }
-    }
-    window.selectedPins = selectedGuestPins;
-
-    var getselectedFeaturePin = function (feature, featureValue) {
-      if (feature.checked) {
-        for (var m = 0; m < window.selectedPins.length; m++) {
-          var arrFeatures = window.selectedPins[m].offer.features;
-          for (var n = 0; n < arrFeatures.length; n++) {
-            if (featureValue === arrFeatures[n]) {
-              selectedFeaturePins.push(window.selectedPins[m]);
+      var getselectedFeaturePin = function (feature, featureValue) {
+        if (feature.checked) {
+          for (var m = 0; m < window.selectedPins.length; m++) {
+            var arrFeatures = window.selectedPins[m].offer.features;
+            for (var n = 0; n < arrFeatures.length; n++) {
+              if (featureValue === arrFeatures[n]) {
+                selectedFeaturePins.push(window.selectedPins[m]);
+              }
             }
           }
         }
+      };
+
+      var wifi = document.querySelector('#filter-wifi');
+      getselectedFeaturePin(wifi, wifi.value);
+
+      var dishwasher = document.querySelector('#filter-dishwasher');
+      getselectedFeaturePin(dishwasher, dishwasher.value);
+
+      var parking = document.querySelector('#filter-parking');
+      getselectedFeaturePin(parking, parking.value);
+
+      var washer = document.querySelector('#filter-washer');
+      getselectedFeaturePin(washer, washer.value);
+
+      var elevator = document.querySelector('#filter-elevator');
+      getselectedFeaturePin(elevator, elevator.value);
+
+      var conditioner = document.querySelector('#filter-conditioner');
+      getselectedFeaturePin(conditioner, conditioner.value);
+
+      if (selectedFeaturePins.length !== 0) {
+        window.selectedPins = selectedFeaturePins;
       }
-    };
 
-    var wifi = document.querySelector('#filter-wifi');
-    getselectedFeaturePin(wifi, wifi.value);
-
-    var dishwasher = document.querySelector('#filter-dishwasher');
-    getselectedFeaturePin(dishwasher, dishwasher.value);
-
-    var parking = document.querySelector('#filter-parking');
-    getselectedFeaturePin(parking, parking.value);
-
-    var washer = document.querySelector('#filter-washer');
-    getselectedFeaturePin(washer, washer.value);
-
-    var elevator = document.querySelector('#filter-elevator');
-    getselectedFeaturePin(elevator, elevator.value);
-
-    var conditioner = document.querySelector('#filter-conditioner');
-    getselectedFeaturePin(conditioner, conditioner.value);
-
-    window.selectedPins = selectedFeaturePins;
-
-    var quantityPins = Math.min(window.selectedPins.length, CONST_QUANTITY_PINS);
-    window.pin.drawPin(window.selectedPins, quantityPins);
+      var quantityPins = Math.min(window.selectedPins.length, CONST_QUANTITY_PINS);
+      window.pin.drawPin(window.selectedPins, quantityPins);
+    }, TIMEOUT);
   };
   mapFilters.addEventListener('change', mapFiltersHandler);
-
 
   window.filters = {
     getPins: getPins,
